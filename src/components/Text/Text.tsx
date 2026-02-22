27a920446572cf859860dbc178c2dac8bac4ba1c
Александр Швецов <shvitc@yandex.ru>
2026-02-22 03:52:49 +0300

    feat: add font roboto semiBold

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -6,7 +6,7 @@ export type TextProps = {
   className?: string;
   view?: 'title' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
   tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'p' | 'span';
-  weight?: 'normal' | 'medium' | 'bold';
+  weight?: 'normal' | 'medium' | 'semiBold' | 'bold';
   children: React.ReactNode;
   color?: 'primary' | 'secondary' | 'accent';
   maxLines?: number;
