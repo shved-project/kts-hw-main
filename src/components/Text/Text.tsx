9ebefc8b9c4107c44c4c79e2a2e0b97810401351
Александр Швецов <shvitc@yandex.ru>
2026-02-23 04:24:25 +0300

    feat: add h2 style variant

--- src/components/Text/Text.tsx
+++ src/components/Text/Text.tsx
@@ -4,7 +4,7 @@ import styles from './Text.module.scss';
 
 export type TextProps = {
   className?: string;
-  view?: 'title' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
+  view?: 'title' | 'title-h2' | 'button' | 'p-20' | 'p-18' | 'p-16' | 'p-14';
   tag?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'div' | 'p' | 'span';
   weight?: 'normal' | 'medium' | 'semiBold' | 'bold';
   children: React.ReactNode;
