4ab76ade2fb0f669c1102339633e76ab58ae4a06
Александр Швецов <shvitc@yandex.ru>
2026-02-26 14:22:13 +0300

    chore: install mobx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import './styles/global.scss';
+import 'config/configureMobX';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
