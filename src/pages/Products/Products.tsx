26504ffa342b5896fe6a0857edf98ea242f7a082
Александр Швецов <shvitc@yandex.ru>
2026-03-02 11:31:41 +0300

    WIP: implement product search functionality

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -2,12 +2,14 @@ import Container from 'components/Container';
 import styles from './Products.module.scss';
 import ProductsText from './components/ProductsText';
 import ProductsList from './components/ProductsList';
+import ProductsForm from './components/ProductsForm';
 
 const Products = () => {
   return (
     <section className={styles.products}>
       <Container>
         <ProductsText />
+        <ProductsForm />
         <ProductsList />
       </Container>
     </section>
