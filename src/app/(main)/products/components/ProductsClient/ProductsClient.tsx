2351359af7feffab5d381373534528240a384fa3
Александр Швецов <shvitc@yandex.ru>
2026-03-10 04:57:28 +0300

    fix: fixed build error

--- src/app/(main)/products/components/ProductsClient/ProductsClient.tsx
+++ src/app/(main)/products/components/ProductsClient/ProductsClient.tsx
@@ -4,6 +4,9 @@ import { useLocalStore } from '@/store/hooks';
 import { ProductsStore, ProductsStoreProvider } from '@/store';
 import ProductsListWrapper from '../ProductsCards/ProductsListWrapper';
 import ProductsFilterWrapper from '../ProductsFilter/ProductsFilterWrapper';
+import { Suspense } from 'react';
+import Loader from '@/components/Loader';
+import styles from '../../Products.module.scss';
 
 const ProductsClient = () => {
   const productsStore = useLocalStore(() => new ProductsStore());
@@ -11,7 +14,9 @@ const ProductsClient = () => {
   return (
     <ProductsStoreProvider store={productsStore}>
       <ProductsFilterWrapper />
-      <ProductsListWrapper />
+      <Suspense fallback={<Loader className={styles.products__loader} />}>
+        <ProductsListWrapper />
+      </Suspense>
     </ProductsStoreProvider>
   );
 };
