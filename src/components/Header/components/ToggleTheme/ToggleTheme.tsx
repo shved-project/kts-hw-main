1e34ef729a082e3fa9559c5f1db90274f6c6f12f
Александр Швецов <shvitc@yandex.ru>
2026-03-11 01:44:07 +0300

    feat: add light/dark theme

--- src/components/Header/components/ToggleTheme/ToggleTheme.tsx
+++ src/components/Header/components/ToggleTheme/ToggleTheme.tsx
@@ -1,19 +1,44 @@
 'use client';
 
-import { useEffect } from 'react';
+import React from 'react';
 import { useThemeStore } from '@/store';
 import { observer } from 'mobx-react-lite';
+import styles from '../../Header.module.scss';
+import Image from 'next/image';
+import lightThemeIcon from '@/assets/icons/light-theme.svg';
+import darkThemeIcon from '@/assets/icons/dark-theme.svg';
+import classNames from 'classnames';
 
 const ToggleTheme = () => {
   const { theme, init, toggle } = useThemeStore();
 
-  useEffect(() => {
+  React.useEffect(() => {
     init();
   }, [init]);
 
+  const isLightTheme = React.useMemo(() => theme === 'light', [theme]);
+
   return (
-    <button onClick={toggle} aria-label="Toggle theme">
-      {theme === 'light' ? 'Dark' : 'Light'}
+    <button
+      className={styles.header__toggle_theme_button}
+      onClick={toggle}
+      aria-label="Toggle theme"
+    >
+      <div className={styles.header__toggle_theme_wrapper}>
+        <div
+          className={classNames(styles.header__toggle_theme_thumb, {
+            [styles['header__toggle_theme_thumb--active']]: isLightTheme,
+          })}
+        >
+          <Image
+            className={styles.header__toggle_theme_icon}
+            src={isLightTheme ? darkThemeIcon : lightThemeIcon}
+            alt="toggle-theme"
+            width={20}
+            height={20}
+          />
+        </div>
+      </div>
     </button>
   );
 };
