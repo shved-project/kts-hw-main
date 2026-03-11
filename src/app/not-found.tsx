2fd0ddf42461feccd77ae7fea51bc73b0c0e86a6
Александр Швецов <shvitc@yandex.ru>
2026-03-11 06:47:38 +0300

    style: add styles for the 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,30 @@
-const notFound = () => {
-  return <h1>404 not found</h1>;
+'use client';
+
+import React from 'react';
+import Text from '@/components/Text';
+import styles from './not-found.module.scss';
+import Button from '@/components/Button';
+import { useRouter } from 'next/navigation';
+
+const NotFound = () => {
+  const router = useRouter();
+
+  const handleClick = React.useCallback(() => {
+    if (window.history.length > 1) {
+      router.back();
+    } else {
+      router.push('/');
+    }
+  }, [router]);
+
+  return (
+    <div className={styles.not_found}>
+      <Text tag="h1" view="title">
+        404 NOT FOUND
+      </Text>
+      <Button onClick={handleClick}>Go back</Button>
+    </div>
+  );
 };
 
-export default notFound;
+export default NotFound;
