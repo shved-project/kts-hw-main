bd11ac1b2d1bfff22808a436e89d0285ac356f0c
Александр Швецов <shvitc@yandex.ru>
2026-03-18 03:44:32 +0300

    feat: redirect unauthenticated user to login from product page

--- src/app/(main)/products/[id]/components/AddToCartButton/AddToCartButton.tsx
+++ src/app/(main)/products/[id]/components/AddToCartButton/AddToCartButton.tsx
@@ -1,11 +1,13 @@
 'use client';
 
 import React from 'react';
-import { useCartStore } from '@/store';
+import { useCartStore, useUserStore } from '@/store';
 import { ProductType } from '@/api/req/products.api';
 import Button from '@/components/Button';
 import styles from './AddToCartButton.module.scss';
 import { observer } from 'mobx-react-lite';
+import { useRouter } from 'next/navigation';
+import routerData from '@/config/routerData';
 
 type AddToCartButtonProps = {
   product: ProductType;
@@ -15,9 +17,15 @@ const AddToCartButton: React.FC<AddToCartButtonProps> = ({ product }) => {
   const { addItem, isInCart } = useCartStore();
   const inCart = isInCart(product.documentId);
 
-  const handleAddToCart = React.useCallback(() => {
-    addItem(product);
-  }, [addItem, product]);
+  const router = useRouter();
+
+  const handleAddToCart = React.useCallback(async () => {
+    const { success, notAuthorized } = await addItem(product);
+
+    if (!success && notAuthorized) {
+      router.push(routerData.login.href);
+    }
+  }, [addItem, product, router]);
 
   return (
     <Button
